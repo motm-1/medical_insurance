0dc8682741f092d7c88d2645b5897d80f3f2997b
Tomas <nahuel568178@gmail.com>
2023-02-28 00:41:00 -0300

    new commit

--- main.py
+++ main.py
@@ -14,12 +14,27 @@ def run():
 
     names_to_tune = [names[r2.index(r2_copy[0])], names[r2.index(r2_copy[1])]]
     print(names_to_tune)
-    parameters = {
-        names_to_tune[0]: '',
-        names_to_tune[1]: ''
-    }
-    after_tuning_results = models.tuning_models(names_to_tune, parameters)
+
+    def tune_models():
+        parameters = {
+            names_to_tune[0]: {
+                'n_neighbors': [3, 5, 7],
+                'weights': ['uniform', 'distance'],
+                'algorithm': ['ball_tree', 'kd_tree', 'brute'],
+                'p': [1, 2]
+            },
+            names_to_tune[1]: {}
+        }
+
+        after_tuning_results = models.tuning_models(names_to_tune, parameters)
+
+        print(f'Scores: {after_tuning_results["scores"]} \n'
+              f'Estimators: {after_tuning_results["estimators"]}')
+
+        return after_tuning_results["cv_results"]
+
+    return pd.DataFrame(tune_models())
 
 
 if __name__ == "__main__":
-    run()
+    run().to_csv('./data/analysis_data.csv')
--- models.py
+++ models.py
@@ -8,26 +8,41 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.svm import SVR
 from sklearn.preprocessing import StandardScaler
 
+
 class Models:
     def __init__(self):
         self.models = [('LiR', LinearRegression()),
-                  ('CART', DecisionTreeRegressor()),
-                  ('kNN', KNeighborsRegressor()),
-                  ('SVR', SVR(gamma='auto', kernel='poly', C=10))]
+                       ('CART', DecisionTreeRegressor()),
+                       ('kNN', KNeighborsRegressor()),
+                       ('SVR', SVR(gamma='auto', kernel='poly', C=10))]
+
+        self.kfold = KFold(n_splits=10, random_state=7, shuffle=True)
+
+        self.X, self.Y = utils.Utils().get_x_y()
+
+        self.X = StandardScaler().fit_transform(self.X)
 
     def models_results(self):
-
-        X, Y = utils.Utils().get_x_y()
-        X = StandardScaler().fit_transform(X)
-        kfold = KFold(n_splits=10, random_state=7, shuffle=True)
         results = []
 
         for name, model in self.models:
-            model_score = cross_val_score(model, X, Y, cv=kfold, scoring='r2')
+            model_score = cross_val_score(model, self.X, self.Y, cv=self.kfold, scoring='r2')
             results.append((name, model_score.mean(), model_score.std()))
 
         return results
 
     def tuning_models(self, names_models, parameters):
         models = {i: j for i, j in self.models if i in names_models}
-        grid = GridSearchCV(models, parameters)
+        scores = []
+        best_estimators = []
+        for key, value in models.items():
+            grid = GridSearchCV(value, parameters[key], cv=self.kfold, scoring='r2').fit(self.X, self.Y)
+            scores.append(np.abs(grid.best_score_))
+            best_estimators.append(grid.best_estimator_)
+            info = grid.cv_results_
+
+        return {
+            'scores': scores,
+            'estimators': best_estimators,
+            'cv_results': info
+        }
